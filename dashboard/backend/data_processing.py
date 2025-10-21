e27a167005fcfc7e03cf9c040d0f9ff82cc4ae52
h4zan <h4zan@hotmail.com>
2025-10-21 16:12:54 +0200

    containarize project

--- dashboard/backend/data_processing.py
+++ dashboard/backend/data_processing.py
@@ -1,30 +1,20 @@
-import os
-from dotenv import load_dotenv
-import snowflake.connector
+import duckdb 
 import pandas as pd 
 from pathlib import Path
 import json
+import os
 
- 
+db_path = os.getenv("DUCKDB_PATH")
+
 def query_job_listings(occupational_field):
 
-    load_dotenv()
-
-    with snowflake.connector.connect(
-        user=os.getenv("SNOWFLAKE_USER"),
-        password=os.getenv("SNOWFLAKE_PASSWORD"),
-        account=os.getenv("SNOWFLAKE_ACCOUNT"),
-        warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
-        database=os.getenv("SNOWFLAKE_DATABASE"),
-        schema=os.getenv("SNOWFLAKE_SCHEMA"),
-        role=os.getenv("SNOWFLAKE_ROLE"),
-    ) as conn:
+    with duckdb.connect(db_path, read_only=True) as conn:
         if occupational_field == "Data/IT":
-            query='SELECT * FROM mart_data_it'
+            query='SELECT * FROM marts.mart_data_it'
         elif occupational_field == "Säkerhet och bevakning":
-            query='SELECT * FROM mart_safety'
+            query='SELECT * FROM marts.mart_safety'
         else:
-            query='SELECT * FROM mart_social_work'
+            query='SELECT * FROM marts.mart_social_work'
         # Execute the query
         df = pd.read_sql(query, conn)
         
