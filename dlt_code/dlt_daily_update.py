dd0e76623863b064c92d89969120e2f621a31873
Efrem <Efremdaniel27@gmail.com>
2025-10-21 13:08:52 +0200

    starting the big data cloud project

--- dlt_code/dlt_daily_update.py
+++ dlt_code/dlt_daily_update.py
@@ -12,7 +12,7 @@ def _get_ads(url, params, headers):
     response.raise_for_status()  # check for http errors
     return json.loads(response.content.decode('utf8'))
 
-@dlt.resource(write_disposition="replace")
+@dlt.resource(table_name = "daily_arbetsformedling_ads", write_disposition="replace")
 def jobsearch_resource():
     date_str = datetime.now()
     today_date = date_str.strftime("%Y-%m-%d")
@@ -30,21 +30,28 @@ def jobsearch_resource():
         yield ad
             
 
-def run_pipeline(table_name):
-    pipeline = dlt.pipeline(
-        pipeline_name="job_ads_stream_daily",
-        destination=dlt.destinations.duckdb(db_path),
-        dataset_name="staging",
-    )
+@dlt.source
+def job_ad_source():
+    return jobsearch_resource()
+
+
+
+
+# def run_pipeline(table_name):
+#     pipeline = dlt.pipeline(
+#         pipeline_name="job_ads_stream_daily",
+#         destination=dlt.destinations.duckdb(db_path),
+#         dataset_name="staging",
+#     )
         
-    load_info = pipeline.run(jobsearch_resource(), table_name=table_name)
-    print(load_info)
+#     load_info = pipeline.run(jobsearch_resource(), table_name=table_name)
+#     print(load_info)
 
 
-if __name__ == "__main__":
-    working_directory = Path(__file__).parent
-    os.chdir(working_directory)
+# if __name__ == "__main__":
+#     working_directory = Path(__file__).parent
+#     os.chdir(working_directory)
 
-    table_name = "daily_arbetsformedling_ads"
+#     table_name = "daily_arbetsformedling_ads"
 
-    run_pipeline(table_name=table_name)
+#     run_pipeline(table_name=table_name)
